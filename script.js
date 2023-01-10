b94a1e181647b37991b64b40b515ab22a413920c
Oscar Martin Mora <moramartin99@gmail.com>
2023-01-10 14:52:51 -0600

    Agregar variables globales

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Variables globales */
+/* --------------------------------------------------------------------------------------------------- */
+const displayContent = document.querySelector(".displayContent");
+const buttonContainer = document.querySelector(".buttonContainer");
+let varA = null;
+let varB = null;
+let operator = null;
+const errMessage = "UwUn't :(";
+/* --------------------------------------------------------------------------------------------------- */
+
